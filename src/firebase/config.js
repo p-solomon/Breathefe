55d46300fa5f0a50290d0ded1d5826c64dbe87e2
Peter Solomon <petersolomon002@gmail.com>
2023-09-19 11:33:52 +0100

    changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,13 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxR3pOda6qHbSiLd9DIKdwfGZLA40Rz_c",
-  authDomain: "breatheproject-a60fa.firebaseapp.com",
-  projectId: "breatheproject-a60fa",
-  storageBucket: "breatheproject-a60fa.appspot.com",
-  messagingSenderId: "159551820491",
-  appId: "1:159551820491:web:54280d3c572090cb26414e",
-  measurementId: "G-GLK3BWZZYH"
+
 };
 
 // Initialize Firebase
